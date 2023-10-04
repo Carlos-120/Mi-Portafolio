c5db5786124fd54dec9c3c5433d66111a62f5480
Carlos.-120 <cpalate18@gmail.com>
2023-10-03 21:04:08 -0500

    chore octava prueva

--- _redirects.js
+++
@@ -1,7 +0,0 @@
-import { redirect } from "@netlify/redirector";
-
-module.exports = redirect([
-  { from: "/aboutme", to: "/home", status: 200 },
-  { from: "/myskills", to: "/home", status: 200 },
-  // Agrega más reglas de redirección según tus rutas necesarias
-]);
