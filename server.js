e17b8b9232cac7733ca4781c15ba02183bac7153
Carlos.-120 <cpalate18@gmail.com>
2023-10-03 20:17:17 -0500

    chore: segunda prueva

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+// No es necesario declarar __filename de nuevo, ya está disponible en Node.js
+
 const express = require("express");
+const { fileURLToPath } = require("url");
 const path = require("path");
+
+// Usar directamente __filename y __dirname
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Resto de tu código...
 
 // Configurar el middleware para servir archivos estáticos desde la carpeta 'build'
 app.use(express.static(path.join(__dirname, "build")));
