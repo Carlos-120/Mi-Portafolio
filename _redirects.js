8d9b0796cc1f2054872c146e8bd82fc5c3d11fd0
Carlos.-120 <cpalate18@gmail.com>
2023-10-03 20:41:59 -0500

    chore: sexta  prueva

--- _redirects.js
+++ _redirects.js
@@ -1,7 +1,7 @@
 import { redirect } from "@netlify/redirector";
 
 module.exports = redirect([
-  { from: "/aboutme", to: "/index.html", status: 200 },
-  { from: "/myskills", to: "/index.html", status: 200 },
+  { from: "/aboutme", to: "/home", status: 200 },
+  { from: "/myskills", to: "/home", status: 200 },
   // Agrega más reglas de redirección según tus rutas necesarias
 ]);
